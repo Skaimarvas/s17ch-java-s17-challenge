59840808a52b3478d19523a97f2da361a10a0143
skaimarvas <skaimarvas@gmail.com>
2024-02-09 21:12:35 +0300

    unversionedfiles

---
+++ s17ch/src/main/java/com/example/s17ch/S17chApplication.java
@@ -0,0 +1,13 @@
+package com.example.s17ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17chApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17chApplication.class, args);
+	}
+
+}
