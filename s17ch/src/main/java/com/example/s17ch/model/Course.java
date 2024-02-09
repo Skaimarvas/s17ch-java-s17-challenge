e14c54b9e327b2bc81a416134d45364835cb4cfe
skaimarvas <skaimarvas@gmail.com>
2024-02-09 21:34:23 +0300

    @AllArgsConstructor @NoArgsConstructor @Data annotations were added. Name, credit and grade fields were created.

--- s17ch/src/main/java/com/example/s17ch/model/Course.java
+++ s17ch/src/main/java/com/example/s17ch/model/Course.java
@@ -1,4 +1,17 @@
 package com.example.s17ch.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Course {
+    private String name;
+    private int credit;
+    private Grade grade;
+
+
 }
