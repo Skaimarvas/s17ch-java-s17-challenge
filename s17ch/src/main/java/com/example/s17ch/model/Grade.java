3385215bd3357048be8db2b30ee108afd5762416
skaimarvas <skaimarvas@gmail.com>
2024-02-09 21:29:06 +0300

    Fields were added and constructor was created in Grade class .

--- s17ch/src/main/java/com/example/s17ch/model/Grade.java
+++ s17ch/src/main/java/com/example/s17ch/model/Grade.java
@@ -1,5 +1,11 @@
 package com.example.s17ch.model;
 
 public class Grade {
+   private int coefficient;
+   private String note;
 
+    public Grade(int coefficient, String note) {
+        this.coefficient = coefficient;
+        this.note = note;
+    }
 }
