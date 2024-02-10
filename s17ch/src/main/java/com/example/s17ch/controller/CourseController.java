3cf9f004dae24e71aad262fdd0cd31ce9948dd1d
skaimarvas <skaimarvas@gmail.com>
2024-02-10 12:51:39 +0300

    RestController, RequestMapping, PostConstruct and GetMapping annotations were added.

--- s17ch/src/main/java/com/example/s17ch/controller/CourseController.java
+++ s17ch/src/main/java/com/example/s17ch/controller/CourseController.java
@@ -1,4 +1,31 @@
 package com.example.s17ch.controller;
 
+import com.example.s17ch.model.Course;
+import com.example.s17ch.model.CourseGpa;
+import com.example.s17ch.model.Grade;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/course")
 public class CourseController {
+    private List<Course> courses;
+
+    @PostConstruct
+    void init(){
+        courses = new ArrayList<>();
+        courses.add(new Course("Economics", 30, new Grade(6,"10")));
+        courses.add(new Course("History", 30, new Grade(6,"10")));
+    }
+
+    @GetMapping
+    public List<Course> findAll(){
+        return courses.stream().toList();
+    }
 }
