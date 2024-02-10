3d1b5776eb12b8e226d2c625360421d1813d02b8
skaimarvas <skaimarvas@gmail.com>
2024-02-10 15:59:09 +0300

    courseId field was added in Course class

--- s17ch/src/main/java/com/example/s17ch/model/Course.java
+++ s17ch/src/main/java/com/example/s17ch/model/Course.java
@@ -1,6 +1,9 @@
 package com.example.s17ch.model;
 
 
+import com.example.s17ch.entity.HighCourseGpa;
+import com.example.s17ch.entity.LowCourseGpa;
+import com.example.s17ch.entity.MediumCourseGpa;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +11,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
 public class Course {
+    private int courseId;
     private String name;
     private int credit;
     private Grade grade;
+
+
+
+
+
 
 
 }
