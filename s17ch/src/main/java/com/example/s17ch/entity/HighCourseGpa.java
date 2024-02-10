253d9c1d07628eb20c5d3f2bbcc7f44e728a085a
skaimarvas <skaimarvas@gmail.com>
2024-02-10 21:17:09 +0300

    old methods were removed

--- s17ch/src/main/java/com/example/s17ch/entity/HighCourseGpa.java
+++ s17ch/src/main/java/com/example/s17ch/entity/HighCourseGpa.java
@@ -2,9 +2,12 @@ package com.example.s17ch.entity;
 
 import com.example.s17ch.model.CourseGpa;
 
+
 public class HighCourseGpa implements CourseGpa {
     @Override
     public int getGpa() {
         return 10;
+
     }
+
 }
