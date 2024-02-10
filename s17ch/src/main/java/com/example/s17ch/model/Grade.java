ceed38c4b9c935f449207eba1595c0f6b7d6890c
skaimarvas <skaimarvas@gmail.com>
2024-02-10 21:16:14 +0300

    @NoArgsConstructor @Data annotations were added

--- s17ch/src/main/java/com/example/s17ch/model/Grade.java
+++ s17ch/src/main/java/com/example/s17ch/model/Grade.java
@@ -1,5 +1,13 @@
 package com.example.s17ch.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * NoArgsConstructor ve Data ile yazmadığım için hata aldım.
+ */
+@NoArgsConstructor
+@Data
 public class Grade {
    private int coefficient;
    private String note;
