7c8c41328f8198783896527523101e1719286bd9
skaimarvas <skaimarvas@gmail.com>
2024-02-11 15:13:05 +0300

    Fields were created and annotations were added

--- s17ch/src/main/java/com/example/s17ch/exceptions/CourseResponse.java
+++ s17ch/src/main/java/com/example/s17ch/exceptions/CourseResponse.java
@@ -1,4 +1,18 @@
 package com.example.s17ch.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CourseResponse {
+    private int status;
+    private String message;
+    private LocalDateTime createdAt;
+
+
 }
