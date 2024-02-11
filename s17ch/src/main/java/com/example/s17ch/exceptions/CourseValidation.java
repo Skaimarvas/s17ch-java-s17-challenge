3da4c50b13bf4f113e873016358e98af243e2d0b
skaimarvas <skaimarvas@gmail.com>
2024-02-11 17:25:38 +0300

    checking if course exist or not with isCourseExist method

--- s17ch/src/main/java/com/example/s17ch/exceptions/CourseValidation.java
+++ s17ch/src/main/java/com/example/s17ch/exceptions/CourseValidation.java
@@ -9,20 +9,30 @@ public class CourseValidation {
     private static final String ID_IS_NOT_VALID = "Course id is not valid";
     private static final String COURSE_IS_NOT_EXIST = "Course with given id is not exist";
     private static final String COURSE_CREDENTIALS_ARE_NOT_VALID = "Course credentials are not valid";
+    private static final String COURSE_IS_ALREADY_EXIST = "Course with given id is already exist";
 
     public static void isIdValid(int id){
 
-            if(id < 0){
+        if(id < 0){
                 throw new CourseException(ID_IS_NOT_VALID + id, HttpStatus.BAD_REQUEST);
             }
 
     }
-    public static void isCourseExist(List<Course> courses, int id){
-        for(Course course : courses){
-            if(!(course.getCourseId() == id)){
-                throw new CourseException(COURSE_IS_NOT_EXIST + id, HttpStatus.NOT_FOUND);
+    public static void isCourseExist(List<Course> courses, int id, boolean boo){
+        if(boo){
+            for(Course course : courses){
+                if(!(course.getCourseId() == id)){
+                    throw new CourseException(COURSE_IS_NOT_EXIST + id, HttpStatus.NOT_FOUND);
+                }
+            }
+        } else {
+            for(Course course : courses){
+                if(!(course.getCourseId() == id)){
+                    throw new CourseException(COURSE_IS_ALREADY_EXIST + id, HttpStatus.BAD_REQUEST);
+                }
             }
         }
+
     }
 
     public static void isCourseCredentialsValid(Course course){
