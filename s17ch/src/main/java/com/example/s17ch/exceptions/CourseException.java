5f18779218e652dce89fb9124d738a80ae6400b6
skaimarvas <skaimarvas@gmail.com>
2024-02-11 15:19:43 +0300

    @Getter annotation was added

--- s17ch/src/main/java/com/example/s17ch/exceptions/CourseException.java
+++ s17ch/src/main/java/com/example/s17ch/exceptions/CourseException.java
@@ -1,7 +1,9 @@
 package com.example.s17ch.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class CourseException extends RuntimeException {
     private HttpStatus status;
     public CourseException(String message, HttpStatus status){
