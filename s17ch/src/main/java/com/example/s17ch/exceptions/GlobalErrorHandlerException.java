1288b4a680f6121e186d8912d3b8bb3658b10c5d
skaimarvas <skaimarvas@gmail.com>
2024-02-11 16:15:06 +0300

    @Slf4j annotation was added

--- s17ch/src/main/java/com/example/s17ch/exceptions/GlobalErrorHandlerException.java
+++ s17ch/src/main/java/com/example/s17ch/exceptions/GlobalErrorHandlerException.java
@@ -1,13 +1,14 @@
 package com.example.s17ch.exceptions;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalErrorHandlerException {
     @ExceptionHandler(CourseException.class)
