9c85b7db3d55e5642e252e3f4458ef68adb3998e
skaimarvas <skaimarvas@gmail.com>
2024-02-11 16:10:45 +0300

    Exception handlers were added

--- s17ch/src/main/java/com/example/s17ch/exceptions/GlobalErrorHandlerException.java
+++ s17ch/src/main/java/com/example/s17ch/exceptions/GlobalErrorHandlerException.java
@@ -1,8 +1,25 @@
 package com.example.s17ch.exceptions;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalErrorHandlerException {
+    @ExceptionHandler(CourseException.class)
+    public ResponseEntity<CourseResponse> handleException(CourseException courseException){
+        CourseResponse courseResponse =
+                new CourseResponse(courseException.getStatus().value(), courseException.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(courseResponse,courseException.getStatus());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CourseResponse> handleException(Exception exception){
+        CourseResponse courseResponse =
+                new CourseResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage(),LocalDateTime.now());
+        return new ResponseEntity<>(courseResponse, HttpStatus.BAD_REQUEST);
+    }
 }
